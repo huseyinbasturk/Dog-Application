341f18f8315d68fd09d0db7afb6b5c3071e34f0c
huseyinbasturk <hb.basturk41@gmail.com>
2022-03-24 23:43:10 +0100

    doggos

---
+++ doggos.js
@@ -0,0 +1,47 @@
+//const BREEDS_URL = 'https://dog.ceo/api/breeds/image/random';
+
+// function addDoggo() {
+//   fetch(BREEDS_URL)
+//   .then(function(response){
+//     return response.json();
+//   })
+//   .then(function(data){
+//     const img = document.createElement('img');
+//     img.src = data.message;
+//     img.alt = 'Cute doggo';
+
+//     document.querySelector('.doggos').appendChild(img);
+
+
+//     // stop showing loading spinner
+//   })
+// }
+
+// document.querySelector('.add-doggo').addEventListener("click", addDoggo);
+
+const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+const select = document.querySelector('.breeds');
+
+
+fetch(BREEDS_URL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    //console.log(data);
+    const breedsObject = data.message;
+    const breedsArray =Object.keys(breedsObject);
+    
+    for (let i = 0; i < breedsArray.length; i++) {
+      const option = document.createElement('option');
+      option.value = breedsArray[i];
+      option.innerText = breedsArray[i];
+      select.appendChild(option);
+        }
+  })
+
+  select.addEventListener("change", function(event) {
+    //console.log(event.target.value);
+    console.log(select.value);
+    console.log('https://dog.ceo/api/breed/${event.target.url}/images/random')
+  })
